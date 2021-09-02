e0cc5fe2fa9bd9d89b419a796e52fa268df56f54
LxndrOghma <Brikman424@gmail.com>
2021-09-02 22:18:35 +0300

    Создаст проект, добавит шрифты, изображения

---
+++ avto-moto/src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MainPage from '../pages/main-page/main-page';
+
+import './App.scss';
+
+function App() {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
---
+++ avto-moto/src/components/pages/main-page/main-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './main-page.scss';
+
+function MainPage() {
+  return <h1>Its works!</h1>;
+}
+
+export default MainPage;
