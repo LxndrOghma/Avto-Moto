3828ac631e8664282235c2b2c94af2ec49b296b5
LxndrOghma <Brikman424@gmail.com>
2021-09-03 21:22:17 +0300

    Добавит кнопку-бургер для мобильного меню

--- avto-moto/src/components/header/main-nav/main-nav.js
+++ avto-moto/src/components/header/main-nav/main-nav.js
@@ -6,7 +6,10 @@ import './main-nav.scss';
 
 function MainNav() {
   return (
-    <nav className='main-nav'>
+    <nav className='main-nav main-nav--closed'>
+      <button className='main-nav__toggle'>
+        <span className='visually-hidden'>Открыть меню</span>
+      </button>
       <ul className='main-nav__list site-menu'>
         {siteMenuLinks.map((link) => <SiteMenuItem key={link} siteMenuLink={link} />)}
       </ul>
