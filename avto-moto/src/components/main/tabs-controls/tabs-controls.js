4808bf6275887b82e4ca8dd38c09d50c99929489
LxndrOghma <Brikman424@gmail.com>
2021-09-11 20:20:08 +0300

    Добавит логику работы в табы

--- avto-moto/src/components/main/tabs-controls/tabs-controls.js
+++ avto-moto/src/components/main/tabs-controls/tabs-controls.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './tabs-controls.scss';
 
-function TabsControls() {
+function TabsControls({activeTab, setActiveTab}) {
+
   return (
     <div className='tabs__controls controls'>
-      <button className='controls__button controls__button--active'>Характеристики</button>
-      <button className='controls__button'>Отзывы</button>
-      <button className='controls__button'>Контакты</button>
+      <button className={`controls__button ${activeTab === 'characteristics' ? 'controls__button--active' : ''}`} onClick={() => setActiveTab('characteristics')}>Характеристики</button>
+      <button className={`controls__button ${activeTab === 'reviews' ? 'controls__button--active' : ''}`} onClick={() => setActiveTab('reviews')}>Отзывы</button>
+      <button className={`controls__button ${activeTab === 'contacts' ? 'controls__button--active' : ''}`} onClick={() => setActiveTab('contacts')}>Контакты</button>
     </div>
   );
 }
+
+TabsControls.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  setActiveTab: PropTypes.func.isRequired,
+};
 
 export default TabsControls;
--- avto-moto/src/components/main/tabs/tabs.js
+++ avto-moto/src/components/main/tabs/tabs.js
@@ -1,18 +1,31 @@
-import React from 'react';
-// import Characteristics from '../characteristics/characteristics';
+import React, { useState } from 'react';
+import Characteristics from '../characteristics/characteristics';
 import Reviews from '../reviews/reviews';
-// import Contacts from '../contacts/contacts';
+import Contacts from '../contacts/contacts';
 import TabsControls from '../tabs-controls/tabs-controls';
 
 import './tabs.scss';
 
 function Tabs() {
+  const [activeTab, setActiveTab] = useState('characteristics');
+
+  const renderActiveTab = (tab) => {
+    switch (tab) {
+      case 'characteristics':
+        return <Characteristics />;
+      case 'contacts':
+        return <Contacts />;
+      case 'reviews':
+        return <Reviews />;
+      default:
+        return <Characteristics />;
+    }
+  };
+
   return (
     <section className='page-main__tabs tabs'>
-      <TabsControls />
-      {/* <Characteristics /> */}
-      {/* <Contacts /> */}
-      <Reviews />
+      <TabsControls activeTab={activeTab} setActiveTab={setActiveTab}/>
+      {renderActiveTab(activeTab)}
     </section>
   );
 }
