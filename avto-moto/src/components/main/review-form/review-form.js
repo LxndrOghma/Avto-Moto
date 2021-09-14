16d659dfdae96e58bf67ae24f2ec742f57662c14
LxndrOghma <Brikman424@gmail.com>
2021-09-15 00:24:15 +0300

    Завершит стилизацию формы отправки отзыва

--- avto-moto/src/components/main/review-form/review-form.js
+++ avto-moto/src/components/main/review-form/review-form.js
@@ -10,7 +10,8 @@ function ReviewForm () {
         <h2 className='review-form__header'>Оставить отзыв</h2>
         <div className='review-form__inputs inputs-block'>
           <label className='inputs-block__label visually-hidden' htmlFor='name'>Имя</label>
-          <input className='inputs-block__input' id='name' name='name' type='text' placeholder='Имя'></input>
+          <input className='inputs-block__input' id='name' name='name' type='text' placeholder='Имя' required></input>
+          <span className='inputs-block__error'>Пожалуйста, заполните поле</span>
           <label className='inputs-block__label visually-hidden' htmlFor='advantages'>Достоинства</label>
           <input className='inputs-block__input' id='advantages' name='advantages' type='text' placeholder='Достоинства'></input>
           <label className='inputs-block__label visually-hidden' htmlFor='disadvantages'>Недостатки</label>
@@ -19,8 +20,12 @@ function ReviewForm () {
             <label className='inputs-block__label'>Оцените товар:</label>
             <RatingInputList />
           </div>
-          <textarea className='inputs-block__comment' name='comment' placeholder='Комментарий'></textarea>
+          <div className='inputs-block__comment-block'>
+            <label className=' visually-hidden' htmlFor='comment'>Комментарий</label>
+            <textarea className='inputs-block__comment' id='comment' name='comment' placeholder='Комментарий' required></textarea>
+          </div>
         </div>
+        <button className='inputs-block__exit-button' />
         <button className='inputs-block__button' type='submit' >Оставить отзыв</button>
       </form>
     </div>
